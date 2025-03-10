86d49dac13f5d44f5bc9360d8da8f0cb8f9b30ed
pragyatiwarii <154691613+pragyatiwarii@users.noreply.github.com>
2025-03-10 22:38:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from processing import preprocess_image, get_contours, warp_perspective, auto_cr
 
 # Load and process image
 image_path = r"C:\Users\pragy\OneDrive\Desktop\Document Scanner 2\Image\image.jpg"
-img, _, _, edges = preprocess_image(image_path)#Muskan...
-biggest = get_contours(edges, img)#Pragya.....
-
+img, _, _, edges = preprocess_image(image_path)
+biggest = get_contours(edges, img)
 # Warp and crop
-warped_img = warp_perspective(img, biggest)#Ritu and Muskan...
+warped_img = warp_perspective(img, biggest)
