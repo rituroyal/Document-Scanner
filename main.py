4846e33d1365d1679a4acae6c19457edcb789f96
rituroyal <rr8883868@gmail.com>
2025-04-14 22:58:54 +0530

    image

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from processing import preprocess_image, get_contours, warp_perspective, auto_crop, enhance_document
 
 # Load and process image
-image_path = r"C:\Users\Asus\OneDrive\Desktop\Scanify\output\final_scanned.jpg"
+image_path = r"C:\Users\Asus\OneDrive\Desktop\Scanify\image\cell_pic.jpg"
 img, _, _, edges = preprocess_image(image_path)
 biggest = get_contours(edges, img)
 # Warp and crop
